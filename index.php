b92bb841fa512f74606648f2a3fd88b7b9ff5afe
Md Foysal Bhuiyan <foysalkhan88672@gmail.com>
2026-08-26 01:34:41 +0600

    product shows at dsiplay

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-     include __DIR__ . '/view/header.php';
- ?>
+include "controller/db.php";
+include __DIR__ . '/view/header.php';
+
+$sql = "SELECT * FROM products WHERE stock > 0";
+$result = mysqli_query($conn, $sql);
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,105 +13,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E Commerce Mall</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="index_style.css"> 
+    <link rel="stylesheet" href="./index_style.css"> 
 </head>
 <body>
-    <header>
-    </header>
-    <div id="hero-section">
-    </div>
-    <div id="shop-section">
-        <div id="box1" class="box">
-            <div class="box-content">
-            <h2>Get your game on</h2>
-            <div id="game-img"></div>
-            <a href="#" class="see-text">see more</a>
-            </div>
-        </div>
-        <div id="box2" class="box">
-            <div class="box-content">
-            <h2>Get your game on</h2>
-            <div id="frypan-img"></div> //frypan image
-            <a href="#" class="see-text">see more</a>
-            </div>
-        </div>
-        <div id="box3" class="box">
-            <div class="box-content">
-            <h2>Get your game on</h2>
-            <div id="cute-img"></div>
-            <a href="#" class="see-text">see more</a>
-            </div>
-        </div>
-        <div id="box4" class="box">
-            <div class="box-content">
-            <h2>Get your game on</h2>
-            <div id="chair-img"></div>
-            <a href="#" class="see-text">see more</a>
-            </div>
-        </div>
 
-        <div id="box1" class="box">
-            <div class="box-content">
-            <h2>Get your game on</h2>
-            <div id="game-img"></div>
-            <a href="#" class="see-text">see more</a>
-            </div>
-        </div>
-        <div id="box2" class="box">
-            <div class="box-content">
-            <h2>Get your game on</h2>
-            <div id="game-img"></div>
-            <a href="#" class="see-text">see more</a>
-            </div>
-        </div>
-        <div id="box3" class="box">
-            <div class="box-content">
-            <h2>Get your game on</h2>
-            <div id="game-img"></div>
-            <a href="#" class="see-text">see more</a>
-            </div>
-        </div>
-        <div id="box4" class="box">
-            <div class="box-content">
-            <h2>Get your game on</h2>
-            <div id="game-img"></div>
-            <a href="#" class="see-text">see more</a>
-            </div>
-        </div>
-
-        <div id="box1" class="box">
-            <div class="box-content">
-            <h2>Get your game on</h2>
-            <div id="game-img"></div>
-            <a href="#" class="see-text">see more</a>
-            </div>
-        </div>
-        <div id="box2" class="box">
-            <div class="box-content">
-            <h2>Get your game on</h2>
-            <div id="game-img"></div>
-            <a href="#" class="see-text">see more</a>
-            </div>
-        </div>
-        <div id="box3" class="box">
-            <div class="box-content">
-            <h2>Get your game on</h2>
-            <div id="game-img"></div>
-            <a href="#" class="see-text">see more</a>
-            </div>
-        </div>
-        <div id="box4" class="box">
-            <div class="box-content">
-            <h2>Get your game on</h2>
-            <div id="game-img"></div>
-            <a href="#" class="see-text">see more</a>
-            </div>
-        </div>
+    <div class="products">
+        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+            <div class="product">
+                <img 
+                    src="media/<?php echo $row['image']; ?>" alt="<?php echo $row['name']; ?>">
+                <h2><?php echo $row['name']; ?></h2>
+                <p><?php echo $row['about']; ?></p>
+                <h3>Price: <?php echo $row['price']; ?></h3>
+                <p>Stock: <?php echo $row['stock']; ?></p></div>
+        <?php } ?>
     </div>
+</body>
     <footer>
             <?php
              include __DIR__ . '/view/footer.php'; 
              ?>
     </footer>
-</body>
 </html>
